ed7bb6363904f11cb9c78724eccbb632d6ad8237
Elena Lubianska <elena.kryzhnua@gmail.com>
2020-04-23 16:38:17 +0300

    start

--- src/com/javaStart/Task5Level1c/Main.java
+++ src/com/javaStart/Task5Level1c/Main.java
@@ -1,16 +1,13 @@
 package com.javaStart.Task5Level1c;
 
 import java.util.Arrays;
-import java.util.Random;
-
-
 
 public class Main {
     public static void main(String[] args) {
-        Random r = new Random();
+
         int [] arr = new int[15];
-        for (int i = 0; i <=15; i++){
-            arr [i] = r.nextInt(15);
+        for (int i = 0; i < arr.length; i++){
+            arr [i] = (int) (Math.random()*11);
 
         }
         System.out.println(Arrays.toString(arr));
